6a6ab545604803be5585679bfd5843492e45772c
Rahul Santra <santrarahul7070@gmail.com>
2023-03-18 12:41:28 +0530

    Nav

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 
 import './App.css';
+import Navbar from './components/Nav';
 
 function App() {
   return (
     <>
-      <h1>hello</h1>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum non sapiente, obcaecati totam voluptas nemo exercitationem eos suscipit itaque deleniti recusandae vitae eius? At incidunt ipsa aliquid fuga eveniet laboriosam.</p>
+      <Navbar/>
     </>
   );
 }
