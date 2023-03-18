45a996c66ceb2d65f9a1adc7b2d45974b2b9536a
Rahul Santra <santrarahul7070@gmail.com>
2023-03-18 10:53:15 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import './App.css';
 function App() {
   return (
     <>
-    <h1>Hello</h1>
+      <h1>hello</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum non sapiente, obcaecati totam voluptas nemo exercitationem eos suscipit itaque deleniti recusandae vitae eius? At incidunt ipsa aliquid fuga eveniet laboriosam.</p>
     </>
   );
 }
